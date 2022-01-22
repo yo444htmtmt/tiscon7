fe8ae5fc40f7b2881247146c8ad024b8cb1a6e62
taien711 <endo0711basket@gmail.com>
2022-01-22 16:38:25 +0900

    入力ミス時のreturnページ変更

--- src/main/java/com/tiscon/controller/EstimateController.java
+++ src/main/java/com/tiscon/controller/EstimateController.java
@@ -124,7 +124,7 @@ public class EstimateController {
 
             model.addAttribute("prefectures", estimateDAO.getAllPrefectures());
             model.addAttribute("userOrderForm", userOrderForm);
-            return "confirm";
+            return "input";
         }
 
         // 料金の計算を行う。
